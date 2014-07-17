895d905ff399b52235483bfa5fb3129600c4ffbf
John O'Malley <jom@nycap.rr.com>
2014-07-16 23:56:48 -0400

    Test by isolating searchable and sortable from each other

--- test/dummy/app/controllers/people_controller.rb
+++ test/dummy/app/controllers/people_controller.rb
@@ -2,4 +2,12 @@ class PeopleController < ApplicationController
 	def index
 		@people = Person.sql_search(params[:search_for]).sql_sort(@sort_by, (@sort_dir))
 	end
+
+	def search_only_index
+		@people = Person.sql_search(params[:search_for])
+	end
+
+	def sort_only_index
+		@people = Person.sql_sort(@sort_by, (@sort_dir))
+	end
 end
