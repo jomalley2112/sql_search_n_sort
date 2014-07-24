5b8ecd0b9553b55a731908f224e7ebf859439a24
John O'Malley <jom@nycap.rr.com>
2014-07-23 23:40:49 -0400

    Get tests working again. All but 2 integration tests and all generator tests passing

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -4,9 +4,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 		   
-
-	include SqlSortSetup
-
-	before_filter :setup_sql_sort, :only => [:index, :sort_only_index]
-		   
 end
