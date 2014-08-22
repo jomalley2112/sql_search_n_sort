b880c0be4f501ebf8c9bfd5e50f95022226cf376
John O'Malley <jom@nycap.rr.com>
2014-08-21 23:01:18 -0400

    clean up sql_searchable_sortable.rb

--- lib/sql_search_n_sort/sql_searchable_sortable.rb
+++ lib/sql_search_n_sort/sql_searchable_sortable.rb
@@ -1,29 +1,18 @@
 module SqlSearchableSortable
 	extend ActiveSupport::Concern
 
-	#TODO: Refactor - There has to be a better way to set and get these values 
-	# like by simply using @@var = ... and ... = @@var instead of 
-	# class_variable_set and _get. May have to use include instead of extend
-	@@default_sort_col = nil #class vars need to be declared
-	@@default_sort_dir = :asc
-	@@sql_search_cols = []
-	@@sql_sort_cols = []
-
   def self.extended(base)
-  	@base = base #for debugging
-  	
   	base.class_eval do
   		attr_accessor :ssns_sortable
 			
-			#:Note remeber these scopes aren't actually running from here. There just
-			# being defined from here...when they get called they don't have access to "base"
-			
+			#:Note remember when debugging from here "base" doesn't exist
 			scope :sql_search, ->(search_for) { where(search_clause(search_for)) }
 			
 			scope :sql_sort, ->(sort_by=nil, dir=nil) do
 				sort_by ||= self.class_variable_get(:@@default_sort_col)
 				dir ||= self.class_variable_get(:@@default_sort_dir) || :asc
-				#This takes care of not only column names as symbols but also as keys of hashes passed to sql_searchable
+				#This takes care of not only column names as symbols but also 
+				# as keys of hashes passed to sql_searchable
 				if  self.class_variable_get(:@@sql_sort_cols).any? { |c| c.is_a?(Hash) ? c.has_key?(sort_by) : c == sort_by }
 					order(sort_by => dir)
 				else
@@ -40,7 +29,6 @@ module SqlSearchableSortable
 	end
 
 	def sql_searchable(*cols)
-		#@is_sql_searchable = true
 		self.class_variable_set(:@@sql_search_cols, (cols ||= []))
 			.select do |c| 
 				col_name = c.is_a?(Hash) ? col.keys.first.to_s : c.to_s
@@ -49,16 +37,14 @@ module SqlSearchableSortable
 	end
 
 	def sql_sortable(*cols)
-		#to make sure @@default_sort_col & @@default_sort_dir get initialized
+		#to ensure @@default_sort_col & @@default_sort_dir get initialized
 		# when no default column is specified for the model we just call default_sql_sort(nil)
 		default_sql_sort(nil)
-		#@base.ssns_sortable = true #debugging
 		self.class_variable_set(:@@sql_sort_cols, cols)
 	end
 
 	def default_sql_sort(col, dir=nil)
 		self.class_variable_set(:@@default_sort_col, col)
-		#if dir is set to :asc just ignore it
 		dir = nil if dir == :asc
 		self.class_variable_set(:@@default_sort_dir, dir)
 	end
