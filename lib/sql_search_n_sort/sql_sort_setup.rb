03fa72fb40040d8bed916a43410d5d1f936580df
John O'Malley <jom@nycap.rr.com>
2014-11-10 22:27:28 -0500

    Deodorize

--- lib/sql_search_n_sort/sql_sort_setup.rb
+++ lib/sql_search_n_sort/sql_sort_setup.rb
@@ -3,10 +3,10 @@ module SqlSortSetup
 	def setup_sql_sort
 		model = controller_name.classify.constantize
     if model.is_a? SqlSearchableSortable
-			if params[:sort_by]
-				@sort_by = (params[:sort_by].split(' ').length > 1) ? 
-											params[:sort_by].split(' ').first.to_sym : params[:sort_by].to_sym
-				dir = (params[:sort_by].split(' ').length > 1) ? params[:sort_by].split(' ')[1].to_sym : nil
+			if sort_by
+				@sort_by = (sort_by.split.length > 1) ? 
+											sort_by.split.first.to_sym : sort_by.to_sym
+				dir = (sort_by.split.length > 1) ? sort_by.split[1].to_sym : nil
 				if [:asc, :desc].include?(dir)
 					@sort_dir = dir
 				else
@@ -17,12 +17,18 @@ module SqlSortSetup
 				end
 			else
 				sort_by_param = model.default_sort_col.to_s
-				sort_by_param += " #{model.default_sort_dir}" unless model.default_sort_dir.nil?
-				#TODO: I'm not sure its adviseable to set a params value like this
+				sort_by_param += " #{model.default_sort_dir}" if model.default_sort_dir
+				#unless model.default_sort_dir.nil?
 				params[:sort_by] = sort_by_param
 			end
 			@sort_dropdown_opts = model.sort_cols_for_dropdown #for the view
 			@sort_dir ||= :asc
 		end
   end
+
+  private
+  	def sort_by
+  		params[:sort_by]
+  	end
 end
+
