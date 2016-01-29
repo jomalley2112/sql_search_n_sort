836adfdf118d199bf8325677fa42c99884140795
John O'Malley <jom@nycap.rr.com>
2016-01-28 22:21:11 -0500

    Switch to MySql. Item specs pass. Update read to include suppress params.

--- lib/sql_search_n_sort/app/helpers/sql_search_n_sort_helper.rb
+++ lib/sql_search_n_sort/app/helpers/sql_search_n_sort_helper.rb
@@ -3,9 +3,7 @@ module SqlSearchNSortHelper
 	#Should only be used for forms performing a GET (which they all should be)
 	def hide_current_params(*suppress)
 		return "" if request.query_parameters.empty?
-		binding.pry
-		#Not sure merge is gonna work here
-		# suppress.merge(SqlSearchNSort.config.suppress_search_params)
+		suppress.concat(SqlSearchNSort.config.suppress_search_params)
 		request.query_parameters.reject { |k, v| suppress.include?(k) }.map do |k, v|
 			hidden_field_tag(k, v)
 		end.join("\n")
