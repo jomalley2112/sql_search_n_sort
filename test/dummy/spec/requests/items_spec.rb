dda0226e3483913e91811c09fe199e793e3c0202
unknown <jomalley@MWL09181401.hq.nyssba.org>
2016-02-03 16:29:36 -0500

    Comments and cleanup

--- test/dummy/spec/requests/items_spec.rb
+++ test/dummy/spec/requests/items_spec.rb
@@ -14,12 +14,10 @@ describe "Items" do
     context "When unwanted parameter names are specified in the sql_search_n_sort config" do
     	before { SqlSearchNSort.config.suppress_search_params = ["restricted_1", "restricted_2"] }
 	    it "doesn't pass them along to the Item#index action" do
-	    	# binding.pry
 	    	visit items_path(good_param: "good", restricted_1: "bad", restricted_2: "bad")
 	    	fill_in("search_for", with: "arbitrary search string")
 	    	click_button "Find"
 	    	sleep 1
-	    	#TODO: This doesn't really address the spec...may bebetter served in a controller spec
 	    	expect(page.current_url).not_to include("bad")
 	    end
 	  end
