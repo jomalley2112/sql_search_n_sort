d3af9d9a612d21d589f33b860703584524f172d8
unknown <jomalley@MWL09181401.hq.nyssba.org>
2016-02-04 16:55:54 -0500

    Test param suppression

--- test/dummy/spec/helpers/sql_search_n_sort_helper_spec.rb
+++ test/dummy/spec/helpers/sql_search_n_sort_helper_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe SqlSearchNSortHelper, type: :helper do
+
 	before(:all) do
     run_generator
   end
@@ -8,14 +9,26 @@ describe SqlSearchNSortHelper, type: :helper do
   after(:all) do 
   	run_destroy
   end
+
 	describe "#hide_current_params" do
-		it "returns the current request parameters as hidden fields" do
+
+		before do
 			controller.request.query_parameters[:param_1] = "1"
 			controller.request.query_parameters[:param_2] = "2"
+		end
+		
+		it "returns the current request parameters as hidden fields" do
 			expect(helper.hide_current_params)
 				.to eq("<input id=\"param_1\" name=\"param_1\" type=\"hidden\" value=\"1\" />\n<input id=\"param_2\" name=\"param_2\" type=\"hidden\" value=\"2\" />")
 		end
-		
+
+		it "does not return any parameters specified in the 'suppress' argument" do
+			controller.request.query_parameters[:unwanted_1] = "unwanted"
+			controller.request.query_parameters[:unwanted_2] = "unwanted"
+			expect(helper.hide_current_params("unwanted_1", "unwanted_2"))
+				.to eq("<input id=\"param_1\" name=\"param_1\" type=\"hidden\" value=\"1\" />\n<input id=\"param_2\" name=\"param_2\" type=\"hidden\" value=\"2\" />")
+		end
+
 	end
 
 
