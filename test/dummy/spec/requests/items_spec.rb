df46cfd73f2c152a1e7fc843cba000885a43f2ee
unknown <jomalley@MWL09181401.hq.nyssba.org>
2016-02-04 16:55:54 -0500

    Test param suppression

--- test/dummy/spec/requests/items_spec.rb
+++ test/dummy/spec/requests/items_spec.rb
@@ -10,15 +10,26 @@ describe "Items" do
   	run_destroy
   end
 
-  describe "Allows exiting url parameters to be suppressed when searching" do
-    context "When unwanted parameter names are specified in the sql_search_n_sort config" do
+  describe "Allows exiting url parameters to be suppressed when searching/sorting" do
+    context "When unwanted parameter names are specified in the suppress_search_params config" do
     	before { SqlSearchNSort.config.suppress_search_params = ["restricted_1", "restricted_2"] }
-	    it "doesn't pass them along to the Item#index action" do
-	    	visit items_path(good_param: "good", restricted_1: "bad", restricted_2: "bad")
-	    	fill_in("search_for", with: "arbitrary search string")
-	    	click_button "Find"
-	    	sleep 1
-	    	expect(page.current_url).not_to include("bad")
+	    context "when ...searching" do
+	    	it "doesn't pass them along to the Item#index action" do
+		    	visit items_path(good_param: "good", restricted_1: "bad", restricted_2: "bad")
+		    	fill_in("search_for", with: "arbitrary search string")
+		    	click_button "Find"
+		    	sleep 1
+		    	expect(page.current_url).not_to include("bad")
+		    end
+	    end
+	    context "...when sorting" do
+	    	it "has no affect on the parameters passed to #index", js: true do
+	    		visit items_path(good_param: "good", restricted_1: "bad", restricted_2: "bad")
+	    		binding.pry
+		    	select("Descr", from: "sort_by")
+		    	sleep 2
+		    	expect(page.current_url).to include("restricted_1")
+	    	end
 	    end
 	  end
 
