2f01fca02efa0805a38ab4454a77f4e2583b2ff5
John O'Malley <jom@nycap.rr.com>
2016-02-08 17:06:40 -0500

    Complete feature for Sorting also.

--- lib/generators/sql_search_n_sort/config/initializers/sql_search_n_sort.rb
+++ lib/generators/sql_search_n_sort/config/initializers/sql_search_n_sort.rb
@@ -1,4 +1,7 @@
 SqlSearchNSort.config do |config|
-	#Add to this array any parameters you don't want the search to pass along . (like "page" in Kaminari)
-	config.suppress_search_params = []
+	#Add to this array any parameters you don't want the search/sort forms to pass along . (like "page" in Kaminari)
+	config.suppress_params = {
+		search: [],
+		sort: []
+		} 
 end
