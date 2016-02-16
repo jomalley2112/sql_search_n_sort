7a8b9949622fc816434c14b163b152eb27aadc09
unknown <jomalley@MWL09181401.hq.nyssba.org>
2016-02-16 16:42:31 -0500

    When passing options to a sql_sortable column everything needs to be within brackets

--- test/dummy/app/models/comment.rb
+++ test/dummy/app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ActiveRecord::Base
 	extend SqlSearchableSortable
 	belongs_to :article
 
-	sql_sortable headline: { joined_table: :article }
+	sql_sortable :commentator, {updated_at: { display_text: "Last Update" }}, {headline: { joined_table: :article }}
 end
