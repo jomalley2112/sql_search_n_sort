cd56505e526bd34e7cb5b5be07ee6ed3c1b1e2a4
John O'Malley <jom@nycap.rr.com>
2016-02-16 21:51:00 -0500

    Specs for duplicate column names on joined tables

--- test/dummy/app/models/comment.rb
+++ test/dummy/app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ActiveRecord::Base
 	extend SqlSearchableSortable
 	belongs_to :article
 
-	sql_sortable :commentator, {updated_at: { display_text: "Last Update" }}, {headline: { joined_table: :article }}
+	sql_sortable :commentator, {updated_at: { display_text: "Last Update" }}, {headline: { joined_table: :article }}, {created_at: { joined_table: :article }}
 end
--- test/dummy/spec/models/comment_spec.rb
+++ test/dummy/spec/models/comment_spec.rb
@@ -12,26 +12,44 @@ describe Comment do
 	 	end
 	end
 
-	describe "Sort" do
-	  describe "Allows sorting by joined model column" do
-			before do
-				(1..5).each do
-					article = FactoryGirl.create(:article, headline: "Some Article Headline #{rand(1..9)}")
-					(1..3).each do
-						FactoryGirl.create(:comment, article: article)
-					end  	
-				end
+	describe "#sql_sort" do
+	  
+		before do
+			Article.record_timestamps = false
+			Comment.record_timestamps = false
+			(1..5).each do
+				article = FactoryGirl.create(:article, headline: "Some Article Headline #{rand(1..9)}", updated_at: Time.now + (rand(1..9)).minutes, created_at: Time.now - (rand(1..9)).minutes)
+				(1..3).each do
+					FactoryGirl.create(:comment, article: article, updated_at: Time.now + (rand(1..9)).minutes, created_at: Time.now - (rand(1..9)).minutes)
+				end  	
 			end
-			
+			expect(Comment.count).to be > 0 #non-trivial for sorting
+		end
+		
+
+		describe "Allows sorting by joined model column" do
 		  it "sorts by article.headline as defined in the model" do
-		  	@comments = Comment.joins(:article).sql_sort("headline", @sort_dir)
-		  	expect(@comments.pluck(:headline)).to eq(@comments.pluck(:headline).sort)
+		  	comments = Comment.joins(:article).sql_sort("headline")
+		  	expect(comments.pluck(:headline)).to eq(comments.pluck(:headline).sort)
+		  end
+		end
+		
+		describe "Column Naming Conflicts" do
+			context "when joined tables share a column name" do
+				let!(:comments_1) { Comment.joins(:article).sql_sort("updated_at") }
+				let!(:comments_2) { Comment.joins(:article).sql_sort("created_at") }
+				describe "sorts by the column from the table specified within the hash (or the base table)" do
+					it "sorts using the comments table for updated_at" do
+						expect(comments_1.pluck("comments.updated_at")).to eq(comments_1.pluck("comments.updated_at").sort)
+					end
+					it "sorts using the articles table for created_at" do
+						expect(comments_2.pluck("articles.created_at")).to eq(comments_2.pluck("articles.created_at").sort)
+					end
+				end
 		  end
 		end
 	end
 	
-	describe "Search" do
-	  pending
-	end
+
  
 end
