a412d03d6286a968a5d1ca1de9f5f9c0069fc538
John OMalley <jom@nycap.rr.com>
2017-10-31 22:11:23 -0400

    Update testing gems and remove require "factory_girl_rails" if Rails.env == 'test’. All green. v2.1.6

--- lib/sql_search_n_sort.rb
+++ lib/sql_search_n_sort.rb
@@ -5,7 +5,6 @@ module SqlSearchNSort
 	require "sql_search_n_sort/sql_sort_setup"
 	require 'sql_search_n_sort/railtie' if defined?(Rails)
 	require 'sql_search_n_sort/exceptions'
-	require "factory_girl_rails" if Rails.env == 'test'
 	require "haml-rails"
 	require "jquery-rails"
 
